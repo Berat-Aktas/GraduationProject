a43b2dc1454747e5f1a13021df69d7151018e7ab
Berat Aktas <berataktas2020@gmail.com>
2024-02-23 13:51:20 +0300

    update appLayout

--- project1/src/components/layout/AppLayout.js
+++ project1/src/components/layout/AppLayout.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import Grid from '@mui/material/Grid';
 import Navbar from '../navbar/Navbar';
 //import Sidebar from '../navbar/Sidebar';
 import { Box } from '@mui/material';
 import Sidebar2 from '../navbar/Sidebar2';
 import Footer from '../navbar/Footer';
+import { Outlet } from 'react-router-dom';
 
 
 const AppLayout = ({ children }) => {
@@ -16,20 +16,40 @@ const AppLayout = ({ children }) => {
     };
 
     return (
-        <>
-            <Box display="flex">
-                <Sidebar2 />
-                <Box flexGrow={1}>
-                    <Navbar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            {children}
-                        </Grid>
-                    </Grid>
+        <Navbar>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    minHeight: "100vh",
+                    flexGrow: 1,
+                    backgroundImage: ``,
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "auto 70vh",
+                    backgroundPosition: "center",
+                    width: "calc(100% - 320px)",
+                }}
+            >
+                <Box component="main" sx={{ px: 3, mt: 10, mb: 3 }}>
+                    <Outlet />
                 </Box>
+                <Footer />
             </Box>
-            <Footer />
-        </>
+        </Navbar>
+        // <>
+        //     <Box display="flex">
+        //         <Sidebar2 />
+        //         <Box flexGrow={1}>
+        //             <Navbar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
+        //             <Grid container spacing={2}>
+        //                 <Grid item xs={12}>
+        //                     {children}
+        //                 </Grid>
+        //             </Grid>
+        //         </Box>
+        //     </Box>
+        //     <Footer />
+        // </>
     )
 }
 
