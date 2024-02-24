8c591b38cedd95a6b573f23eca1d441024054e97
Berat Aktas <119859743+Berat-Aktas@users.noreply.github.com>
2024-02-25 02:54:10 +0300

    Geliştirme

--- project1/src/pages/Auth/ForgotPassword.js
+++ project1/src/pages/Auth/ForgotPassword.js
@@ -7,14 +7,22 @@ const ForgotPassword = () => {
 
     const navigate = useNavigate();
 
-  return (
-    <>
-    <Box>
-    <Avatar sx={{ m: 1, bgcolor: "success.main" }}  ></Avatar>
-    ForgotPassword
-    </Box>
-    </>
-  )
+    return (
+        <>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: 'center',
+                    alignItems: "center",
+                    height: "100%",
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: "success.main" }}  ></Avatar>
+                ForgotPassword
+            </Box>
+        </>
+    )
 }
 
 export default ForgotPassword;
