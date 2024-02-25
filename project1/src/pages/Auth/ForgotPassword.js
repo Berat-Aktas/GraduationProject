75340a14e4c020fbd073170de36b79a6cf163842
Berat Aktas <berataktas2020@gmail.com>
2024-02-25 16:26:57 +0300

    Forgot PAssword sayfası

--- project1/src/pages/Auth/ForgotPassword.js
+++ project1/src/pages/Auth/ForgotPassword.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { Avatar, Box } from '@mui/material';
+import React, {useState} from 'react';
+import {
+    Avatar,
+    Box,
+    Typography
+} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import * as yup from "yup";
+import { Form, FormikProvider, useFormik } from 'formik';
+import SyncLockIcon from '@mui/icons-material/SyncLock';
+import SaveIcon from '@mui/icons-material/Save';
+import { LoadingButton } from '@mui/lab';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 
 const ForgotPassword = () => {
 
-
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    // const {user} = useAuth();
     return (
         <>
             <Box
@@ -17,7 +31,22 @@ const ForgotPassword = () => {
                 }}
             >
                 <Avatar sx={{ m: 1, bgcolor: "success.main" }}  ></Avatar>
-                ForgotPassword
+                <Typography component="h1" variant='h5' align='center' >
+                    Forgot Password
+                </Typography>
+                <Box sx={{ alignSelf: "stretch", mt: 1 }}>
+                <LoadingButton
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                                color="success"
+                                loading={loading}
+                                startIcon={<SaveIcon />}
+                            >
+                                Kaydet
+                            </LoadingButton>
+                </Box>
             </Box>
         </>
     )
