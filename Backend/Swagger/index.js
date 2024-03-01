1de5ca1882ee1b61938e5e387b11e8670b57ab19
Berat Aktas <119859743+Berat-Aktas@users.noreply.github.com>
2024-03-01 12:42:47 +0300

    Swagger Oluşturuldu

---
+++ Backend/Swagger/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
+// Hız sınırlama middleware'inin konfigürasyonu
+const limiter = rateLimit({
+    windowMs:  1000, // 1 saniye
+    max: 100, // bir IP adresinden 1 saniyede Maksimum 100 istek
+    message:
+    "Try later on"
+});
+
+swaggerDocument = require("./swagger.json");
+
+
+const app = express();
+app.use("api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//START swagger-ui
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(limiter);
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+
+app.listen(8000);
