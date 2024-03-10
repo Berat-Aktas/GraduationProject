37b8db76b92a346c36b8fdc9c144135fe2d1d868
Berat Aktas <berataktas2020@gmail.com>
2024-03-10 17:07:03 +0300

    geliştirme

--- project1/src/pages/Auth/ForgotPassword.js
+++ project1/src/pages/Auth/ForgotPassword.js
@@ -1,8 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
     Avatar,
     Box,
-    Typography
+    TextField,
+    Typography,
+    Link,
+    Grid,
 } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import * as yup from "yup";
@@ -12,6 +15,7 @@ import SaveIcon from '@mui/icons-material/Save';
 import { LoadingButton } from '@mui/lab';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import PasswordIcon from '@mui/icons-material/Password';
 
 
 const ForgotPassword = () => {
@@ -30,22 +34,45 @@ const ForgotPassword = () => {
                     height: "100%",
                 }}
             >
-                <Avatar sx={{ m: 1, bgcolor: "success.main" }}  ></Avatar>
+                <Avatar sx={{ m: 1, bgcolor: "success.main" }}  >
+                    <PasswordIcon />
+                </Avatar>
                 <Typography component="h1" variant='h5' align='center' >
                     Forgot Password
                 </Typography>
                 <Box sx={{ alignSelf: "stretch", mt: 1 }}>
-                <LoadingButton
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                                color="success"
-                                loading={loading}
-                                startIcon={<SaveIcon />}
+
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        autoComplete="mail-addres"
+                        label="Mail Adresiniz"
+                    />
+                    <LoadingButton
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                        color="success"
+                        loading={loading}
+                        startIcon={<SaveIcon />}
+                    >
+                        Kaydet
+                    </LoadingButton>
+                    <Grid alignItems="center">
+                        <Grid item xs>
+                            or {' '}
+                            <Link
+                                variant='body2'
+                                color="secondary"
+                                onClick={() => { navigate('/login') }}
+                                style={{ cursor: "pointer" }}
                             >
-                                Kaydet
-                            </LoadingButton>
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                    </Grid>
                 </Box>
             </Box>
         </>
