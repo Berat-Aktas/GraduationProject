ab6fa60bb323d1f243dc7f2ce6db820aaf4a9ea0
Berat Aktas <berataktas2020@gmail.com>
2024-03-11 14:42:29 +0300

    update

--- project1/src/components/layout/AuthLayout.js
+++ project1/src/components/layout/AuthLayout.js
@@ -1,70 +1,83 @@
 import React from 'react';
 import { Box, Grid, Paper, Stack, Typography } from '@mui/material';
 import { Outlet } from 'react-router-dom';
-import bankWiseLogo from '../../assets/2..png';
+// import bankWiseLogo from '../../assets/2..png';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+
 
 
 const AuthLayout = () => {
   return (
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <Grid
-          item
-          sm={12}
-          md={5}
-          component={Paper}
-          elevation={6}
-          square
-          sx={{ display: "flex", flexDirection: "column", height: "inherit" }}
+    <Grid container component="main" sx={{ height: "100vh" }}>
+      <Grid
+        item
+        sm={12}
+        md={5}
+        component={Paper}
+        elevation={6}
+        square
+        sx={{ display: "flex", flexDirection: "column", height: "inherit" }}
+      >
+        <Box
+          boxShadow={1}
+          sx={{
+            backgroundColor: "primary.main",
+            color: "#fff",
+            padding: ".25rem",
+            height: { xs: "56px", sm: "64px" },
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
         >
-          <Box
-            boxShadow={1}
-            sx={{
-              backgroundColor: "primary.main",
-              color: "#fff",
-              padding: ".25rem",
-              height: { xs: "56px", sm: "64px" },
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center"
-            }}
+          {/* LOGO */}
+          {/* <Typography
+            height={{ xs: 35, sm: 40 }}
           >
-            {/* LOGO */}
-            <Typography
-              height={{ xs: 35, sm: 40 }}
-            >
-              <img
+            <img
                 src={bankWiseLogo}
                 alt="BankWise LOGO"
                 style={{ height: "inherit" }}
               />
-            </Typography>
-          </Box>
-          <Box sx={{
-            mb: 2,
-            mx: 4,
-            flexGrow: "1",
-          }}
+          </Typography> */}
+          <Typography
+            sx={{
+              display: 'flex', // Flex kutu yapısını etkinleştir
+              alignItems: 'center', // İçerikleri dikey eksen boyunca merkezle
+              justifyContent: 'center', // İçerikleri yatay eksen boyunca merkezle
+              height: { xs: 35, sm: 40 }
+            }}
           >
-            <Outlet />
-          </Box>
-          <Copyright />
-        </Grid>
-
-        <Grid
-          item
-          sm={false}
-          sx={{
-            backgroundImage: ``,
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
+            <AccountBalanceIcon sx={{ mr: 1 }} /> {/* Sağ tarafına margin ekle */}
+            BankWise
+          </Typography>
+        </Box>
+        <Box sx={{
+          mb: 2,
+          mx: 4,
+          flexGrow: "1",
+        }}
+        >
+          <Outlet />
+        </Box>
+        <Copyright />
       </Grid>
+
+      <Grid
+        item
+        sm={false}
+        sx={{
+          backgroundImage: ``,
+          backgroundRepeat: "no-repeat",
+          backgroundColor: (t) =>
+            t.palette.mode === "light"
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+    </Grid>
   )
 }
 
@@ -79,11 +92,11 @@ function Copyright(props) {
       sx={{ backgroundColor: "primary.main", color: "#fff" }}
       p={2}
     >
-      <Typography 
-      variant='body2' align='center' {...props}
+      <Typography
+        variant='body2' align='center' {...props}
       >
-      {"Copyright © "}
-      {new Date().getFullYear()}{" "}{"BankWise"}
+        {"Copyright © "}
+        {new Date().getFullYear()}{" "}{"BankWise"}
       </Typography>
     </Stack>
   );
