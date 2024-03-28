7fe88c0db6b2cf8c9657dc81fd85f139f4d84cbd
Berat Aktas <berataktas2020@gmail.com>
2024-03-28 23:28:22 +0300

    Backend => Auth
    BackendAuth yazmaya başladım

--- project1/src/api/api.js
+++ project1/src/api/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://api.orneginiz.com'; // API'nizin temel URL'si
+const API_BASE_URL = ''; // API'nizin temel URL'si
+//https://api.orneginiz.com
+
 
 // Profil verilerini çekme fonksiyonu
 export const fetchProfileData = () => {
-  return axios.get(`${API_BASE_URL}/kullanici-profilleri`);
+  return axios.get(`${API_BASE_URL}/profile`);
 };
